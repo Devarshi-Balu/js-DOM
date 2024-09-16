70c1c70173f086967ea231c899e291184616cec6
devarshi-balu <studious.devarshibalu@gmail.com>
2024-09-16 11:15:34 +0530

    addition of btnFlash code

--- simon say game/app.js
+++ simon say game/app.js
@@ -4,6 +4,8 @@ let gameSeq = [];
 let started = false;
 let level = 0;
 
+let colors = ['red', 'yellow', 'green', 'purple'];
+
 h2 = document.querySelector('h2');
 
 document.addEventListener('keydown', function(event) {
@@ -19,6 +21,12 @@ function levelUp() {
     level++;
     h2.innerText = `level ${level}`;
 
+    let randomIdx = Math.floor(Math.random() * 4);
+    let randomColor = colors[randomIdx];
+
+    let button = document.querySelector(`.${randomColor}`);
+    btnFlash(button);
+    
 }
 
 function btnFlash(btn) {
@@ -27,5 +35,4 @@ function btnFlash(btn) {
         btn.classList.remove("flash");
     },0.225*1000);
 }
-
 
